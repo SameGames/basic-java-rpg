e5144339c1205f9dc27e7b31c1d11b405e652cef
Samsonium <tdesu@vk.com>
2023-02-22 02:25:20 +0300

    Implemented game loop in game panel class

--- src/main/java/ru/samegames/GamePanel.java
+++ src/main/java/ru/samegames/GamePanel.java
@@ -2,10 +2,11 @@ package ru.samegames;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int orginalTileSize = 16;
     final int scale = 3;
 
@@ -15,9 +16,36 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
+    Thread gameThread;
+
     public GamePanel() {
         setPreferredSize(new Dimension(screenWidth, screenHeight));
         setDoubleBuffered(true);
         setBackground(Color.black);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void update() {
+        // TODO
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Graphics2D g2 = (Graphics2D)g;
+    }
+    
+    // Thread task runner
+    @Override
+    public void run() {
+        while (gameThread != null) {
+            update(getGraphics());
+            repaint();
+        }
     }
 }
